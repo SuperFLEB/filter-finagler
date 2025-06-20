76bd82128e7a9b9e346157550125aa26e8d1853d
FLEB <voterudy@voterudy.org>
2025-06-20 00:21:13 -0400

    Skip namespace-related tests in jsdom environment because that is not supported

--- tests/util/xpath.test.ts
+++ tests/util/xpath.test.ts
@@ -1,13 +1,7 @@
-import { expect, describe, it } from "vitest";
+import {describe, expect, it} from "vitest";
 import xpath from "@/util/xpath.ts";
-import {Namespaces} from "../../src/constants";
 
 describe("xpath", () => {
-	it("Fails when not all namespace prefixes are defined", () => {
-		const doc = document.implementation.createDocument("http://www.w3.org/XML/1998/namespace", "");
-		expect(() => xpath(doc, "//a:foo/b:bar", { a: "http://example.com" })).toThrowError()
-	});
-
 	it("Finds an element with the default namespace", () => {
 		const doc = document.implementation.createDocument("http://www.w3.org/XML/1998/namespace", "");
 		const docElement = doc.appendChild(doc.createElement("documentElement"));
@@ -34,7 +28,14 @@ describe("xpath", () => {
 		docElement.appendChild(doc.createElement("nothing"));
 		expect(xpath(doc, "//something", {}, true)).toEqual([elements[0]]);
 	});
+});
 
+// Namespaces are not supported by JSDom -- https://github.com/jsdom/jsdom/issues/2997
+describe.skipIf("jsdom" in globalThis)("xpath namespaces (browser only)", () => {
+	it("Fails when not all namespace prefixes are defined", () => {
+		const doc = document.implementation.createDocument("http://www.w3.org/XML/1998/namespace", "");
+		expect(() => xpath(doc, "//a:foo/b:bar", {a: "http://example.com"})).toThrowError();
+	});
 	it("Finds only elements with the matching namespace", () => {
 		const doc = document.implementation.createDocument("http://www.w3.org/XML/1998/namespace", "");
 		const docElement = doc.appendChild(doc.createElement("documentElement"));
@@ -48,7 +49,10 @@ describe("xpath", () => {
 		];
 		docElement.append(...elements);
 		docElement.appendChild(doc.createElement("nothing"));
-		expect(xpath(doc, "//n:something", { x: "http://www.w3.org/XML/1998/namespace", n: "http://example.com" })).toEqual([elements[1], elements[2]]);
+		expect(xpath(doc, "//n:something", {
+			x: "http://www.w3.org/XML/1998/namespace",
+			n: "http://example.com"
+		})).toEqual([elements[1], elements[2]]);
 	});
 
 	it("Finds only the first element with the matching namespace when the firstOnly flag is set", () => {
@@ -64,6 +68,10 @@ describe("xpath", () => {
 		];
 		docElement.append(...elements);
 		docElement.appendChild(doc.createElement("nothing"));
-		expect(xpath(doc, "//n:something", { x: "http://www.w3.org/XML/1998/namespace", n: "http://example.com" }, true)).toEqual([elements[1]]);
+		expect(xpath(doc, "//n:something", {
+			x: "http://www.w3.org/XML/1998/namespace",
+			n: "http://example.com"
+		}, true)).toEqual([elements[1]]);
 	});
-})
+});
+
